d2731d147dae64d72029cad23033bc6a16d8e965
alexcoca <alexcoca23@yahoo.co.uk>
2025-11-11 21:37:43 +0000

    Update: add dataloder utils

---
+++ src/playground/dataloader.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+
+import torch
+from tiktoken import Encoding, get_encoding
+from torch.utils.data import DataLoader, Dataset
+
+from playground.data_utils import load_text
+
+
+def chunk_text(
+    token_ids: list[int], max_len: int, stride: int
+) -> tuple[list[torch.Tensor], list[torch.Tensor]]:
+
+    context, targets = [], []
+    for i in range(0, len(token_ids) - max_len, stride):
+        input_chunk = token_ids[i : i + max_len]
+        context.append(torch.tensor(input_chunk))
+        target_chunk = token_ids[i + 1 : i + max_len + 1]
+        targets.append(torch.tensor(target_chunk))
+
+    return context, targets
+
+
+class GPTDataset(Dataset):
+
+    def __init__(
+        self, path: str | Path, tokenizer: Encoding, max_length: int, stride: int
+    ):
+        text = load_text(path)
+        token_ids = tokenizer.encode(text)
+        self.input_ids, self.target_ids = chunk_text(token_ids, max_length, stride)
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, item: int) -> tuple[torch.Tensor, torch.Tensor]:
+        return self.input_ids[item], self.target_ids[item]
+
+
+def get_test_dataloader(dataset: Dataset, batch_size: int = 1, num_workers: int = 0):
+    return DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+    )
+
+
+def get_train_dataloader(
+    dataset: Dataset,
+    batch_size: int = 1,
+    num_workers: int = 0,
+    drop_last: bool = True,
+):
+    return DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        drop_last=drop_last,
+        shuffle=True,
+    )
+
+
+def get_dataset(
+    path: str | Path,
+    max_length: int = 256,
+    stride: int = 128,
+):
+    tokenizer = get_encoding("gpt2")
+    return GPTDataset(path, tokenizer, max_length, stride)

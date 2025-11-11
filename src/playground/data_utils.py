21a040921f1a399c08d339fbb72ac1d883635d48
alexcoca <alexcoca23@yahoo.co.uk>
2025-11-11 21:22:10 +0000

    Add: data processing code

---
+++ src/playground/data_utils.py
@@ -0,0 +1,48 @@
+import logging
+import urllib.request
+from pathlib import Path
+
+from tiktoken import Encoding
+
+logger = logging.getLogger(__name__)
+
+
+def open_url(url: str, fname: str | Path) -> tuple:
+    return urllib.request.urlretrieve(url, fname)
+
+
+def make_absolute(pth: str | Path) -> Path:
+    if isinstance(pth, str):
+        return Path(pth).absolute()
+    return pth.absolute()
+
+
+def load_text(pth: str | Path) -> str:
+    pth = make_absolute(pth)
+
+    with open(pth, encoding="utf-8") as f:
+        text = f.read()
+    logger.info(f"Read file: {pth}")
+    return text
+
+
+def dump_text(text: str, pth: str | Path) -> str:
+    pth = make_absolute(pth)
+
+    with open(pth, "w", encoding="utf-8") as f:
+        f.write(text)
+    logger.info(f"Wrote file {pth}")
+
+
+def train_val_split(
+    text: str,
+    tokenizer: Encoding,
+    train_ratio: float = 0.9,
+) -> tuple[str, str]:
+
+    encoded = tokenizer.encode(text)
+    split_idx = int(len(encoded) * train_ratio)
+    train_tok, val_tok = encoded[:split_idx], encoded[split_idx:]
+    train_text = tokenizer.decode(train_tok)
+    val_text = tokenizer.decode(val_tok)
+    return train_text, val_text

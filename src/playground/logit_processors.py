36f06fc6729cd68f4a5dc9cbaac0dc80672b8ac0
alexcoca <alexcoca23@yahoo.co.uk>
2025-11-14 12:20:34 +0000

    WIP: logit processors

---
+++ src/playground/logit_processors.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+import torch
+from torchtyping import TensorType
+
+
+class LogitsProcessor(ABC):
+    @abstractmethod
+    def __call__(self, logits: torch.Tensor) -> torch.Tensor:
+        """Process logits before sampling."""
+        pass
+
+
+class TemperatureLogitsProcessor(LogitsProcessor):
+    def __init__(self, temperature: float = 1.0):
+        self.temperature = temperature
+
+    def __call__(self, logits: TensorType["B", "L", "V"]):
+        return logits / self.temperature
+
+
+class TopKLogitsProcessor(LogitsProcessor):
+    def __init__(self, top_k: int):
+        self.top_k = top_k
+
+    def __call__(self, logits: TensorType["B", "L", "V"]):
+        top_k_values, top_k_indices = torch.topk(logits, self.top_k, dim=-1)
+        logits_filtered = torch.full_like(logits, torch.finfo(logits.dtype).min)
+        logits_filtered.scatter_(-1, top_k_indices, top_k_values)
+        return logits_filtered
